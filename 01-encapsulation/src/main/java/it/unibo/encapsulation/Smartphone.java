c372daf22c119f22687d06ae4f9577055b09fac6
ZY <yuhang.zhu@studio.unibo.it>
2024-10-29 10:35:25 +0100

    Add getter for Smartphone.java

--- 01-encapsulation/src/main/java/it/unibo/encapsulation/Smartphone.java
+++ 01-encapsulation/src/main/java/it/unibo/encapsulation/Smartphone.java
@@ -9,14 +9,14 @@ public class Smartphone {
     private static final int DEF_RAM_SIZE = 8192;
     private static final int DEF_N_CPU = 8;
 
-    public final int cpuCount;
-    public final int ram;
-    public final int sdSize;
-    public final String brand;
-    public final String model;
-    public final boolean hasGPS;
-    public final boolean has3G;
-    public final boolean hasNFC;
+    private final int cpuCount;
+    private final int ram;
+    private final int sdSize;
+    private final String brand;
+    private final String model;
+    private final boolean hasGPS;
+    private final boolean has3G;
+    private final boolean hasNFC;
 
     public Smartphone(
         final int cpuCount,
@@ -38,6 +38,49 @@ public class Smartphone {
         this.hasNFC = hasNFC;
     }
 
+    public int getCpuCount() {
+        return this.cpuCount;
+    }
+
+    public int getRam() {
+        return this.ram;
+    }
+
+    public int getSdSize() {
+        return this.sdSize;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public boolean getHasGPS() {
+        return this.hasGPS;
+    }
+
+    public boolean getHas3G() {
+        return this.has3G;
+    }
+
+    public boolean getHasNFC() {
+        return this.hasNFC;
+    }
+
+    public void printStringRep() {
+        System.out.println("n CPU(s): " + cpuCount);
+        System.out.println("RAM amount: " + ram);
+        System.out.println("SD size: " + sdSize);
+        System.out.println("brand: " + brand);
+        System.out.println("model: " + model);
+        System.out.println("hasGPS: " + hasGPS);
+        System.out.println("has3G: " + has3G);
+        System.out.println("hasNFC: " + hasNFC + "\n");
+    }
+
     public Smartphone(final String brand, final String model) {
         this(brand, model, DEF_SD_SIZE);
     }
@@ -52,16 +95,5 @@ public class Smartphone {
 
     public Smartphone(final String brand, final String model, final boolean hasGPS, final boolean has3G) {
         this(DEF_N_CPU, DEF_RAM_SIZE, DEF_SD_SIZE, brand, model, hasGPS, has3G, DEF_HAS_NFC);
-    }
-
-    public void printStringRep() {
-        System.out.println("n CPU(s): " + cpuCount);
-        System.out.println("RAM amount: " + ram);
-        System.out.println("SD size: " + sdSize);
-        System.out.println("brand: " + brand);
-        System.out.println("model: " + model);
-        System.out.println("hasGPS: " + hasGPS);
-        System.out.println("has3G: " + has3G);
-        System.out.println("hasNFC: " + hasNFC + "\n");
     }
 }
